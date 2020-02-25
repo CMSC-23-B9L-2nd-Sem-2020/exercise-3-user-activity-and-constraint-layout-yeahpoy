055e1fadacd6fc6cee7c4935f6bbdcadda6a07b0
yeahpoy <hplorente@gmail.com>
2020-02-25 20:01:28 +0800

    feature: add win indicator & win text

--- app/src/main/java/com/example/lorente_exer3_b9l/MainActivity.kt
+++ app/src/main/java/com/example/lorente_exer3_b9l/MainActivity.kt
@@ -9,16 +9,20 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
+    private var numOfClicks = 0
+    private lateinit var showCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         var board : Array<Array<Int>> = arrayOf(
+        showCount = findViewById<TextView>(R.id.clickNumber)
+
+        var board : Array<Array<Int>> = arrayOf(
              arrayOf(1,1,1,1,1),
              arrayOf(1,1,1,1,1),
              arrayOf(1,1,1,1,1),
@@ -26,12 +30,26 @@ class MainActivity : AppCompatActivity() {
              arrayOf(1,1,1,1,1)
         )
 
+        val boxList: List<List<Int>> = listOf(
+            listOf(R.id.tile_11, R.id.tile_12, R.id.tile_13, R.id.tile_14, R.id.tile_15),
+            listOf(R.id.tile_21, R.id.tile_22, R.id.tile_23, R.id.tile_24, R.id.tile_25),
+            listOf(R.id.tile_31, R.id.tile_32, R.id.tile_33, R.id.tile_34, R.id.tile_35),
+            listOf(R.id.tile_41, R.id.tile_42, R.id.tile_43, R.id.tile_44, R.id.tile_45),
+            listOf(R.id.tile_51, R.id.tile_52, R.id.tile_53, R.id.tile_54, R.id.tile_55)
+        )
+
 
         findViewById<Button>(R.id.change_button).setOnClickListener{
             changeNickname(it)
         }
 
-        setListeners(board)
+        setListeners(board,boxList)
+
+
+        findViewById<Button>(R.id.retry_button).setOnClickListener{
+            retry(board, boxList)
+        }
+
 
     }
 
@@ -46,14 +64,7 @@ class MainActivity : AppCompatActivity() {
         nicknameTextView.visibility = View.VISIBLE
     }
 
-    private fun setListeners(board: Array<Array<Int>>) {
-        val boxList: List<List<Int>> = listOf(
-            listOf(R.id.tile_11, R.id.tile_12, R.id.tile_13, R.id.tile_14, R.id.tile_15),
-            listOf(R.id.tile_21, R.id.tile_22, R.id.tile_23, R.id.tile_24, R.id.tile_25),
-            listOf(R.id.tile_31, R.id.tile_32, R.id.tile_33, R.id.tile_34, R.id.tile_35),
-            listOf(R.id.tile_41, R.id.tile_42, R.id.tile_43, R.id.tile_44, R.id.tile_45),
-            listOf(R.id.tile_51, R.id.tile_52, R.id.tile_53, R.id.tile_54, R.id.tile_55)
-        )
+    private fun setListeners(board: Array<Array<Int>>, boxList: List<List<Int>>) {
 
         for (row: Int in (0..4)) {
             for(column: Int in (0..4)) {
@@ -63,31 +74,39 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
     }
 
     private fun changeColor(view: View, board: Array<Array<Int>>, boxList: List<List<Int>>, row : Int, column: Int) {
-
-//        var id = resources.getResourceEntryName(view.id)
-//        var id_split = id.split("_")
-//        var index = id_split[1]
-//        Toast.makeText(this, index, Toast.LENGTH_LONG).show()
-//
-//        // Getting the row and column index from the
-//        var rowChar = index[0]
-//        var columnChar = index[1]
-//
-//        var row = rowChar.toInt()
-//        var column = columnChar.toInt()
         if(board[row][column] == 1) {
             view.setBackgroundColor(Color.BLACK)
             board[row][column] = 0
+            flipLights(board,boxList,row,column)
         }
         else{
             view.setBackgroundColor(Color.WHITE)
             board[row][column] = 1
+            flipLights(board,boxList,row,column)
         }
 
-        flipLights(board,boxList,row,column)
+        var win = winChecker(board)
+        if(win == 1) {
+            for(i in 0..4) {
+                for(j in 0..4) {
+                    findViewById<TextView>(boxList[i][j]).visibility = View.INVISIBLE
+                }
+            }
+            findViewById<TextView>(R.id.retry_button).visibility = View.GONE
+            findViewById<TextView>(R.id.win_text).visibility = View.VISIBLE
+
+        }
+
+        numOfClicks++
+
+        if(showCount != null) {
+            showCount.setText(numOfClicks.toString())
+        }
+
 
     }
 
@@ -97,8 +116,10 @@ class MainActivity : AppCompatActivity() {
         try {
             if (board[row][column + 1] == 1) {
                 findViewById<TextView>(boxList[row][column + 1]).setBackgroundColor(Color.BLACK)
+                board[row][column + 1] = 0
             } else {
                 findViewById<TextView>(boxList[row][column + 1]).setBackgroundColor(Color.WHITE)
+                board[row][column + 1] = 1
             }
         }catch (e: Exception) {}
 
@@ -106,8 +127,10 @@ class MainActivity : AppCompatActivity() {
         try {
             if (board[row][column - 1] == 1) {
                 findViewById<TextView>(boxList[row][column - 1]).setBackgroundColor(Color.BLACK)
+                board[row][column - 1] = 0
             } else {
                 findViewById<TextView>(boxList[row][column - 1]).setBackgroundColor(Color.WHITE)
+                board[row][column - 1] = 1
             }
         }catch (e: Exception) {}
 
@@ -115,8 +138,10 @@ class MainActivity : AppCompatActivity() {
         try {
             if (board[row + 1][column] == 1) {
                 findViewById<TextView>(boxList[row + 1][column]).setBackgroundColor(Color.BLACK)
+                board[row + 1][column] = 0
             } else {
-                findViewById<TextView>(boxList[row + 1][column + 1]).setBackgroundColor(Color.WHITE)
+                findViewById<TextView>(boxList[row + 1][column]).setBackgroundColor(Color.WHITE)
+                board[row + 1][column] = 1
             }
         }catch (e: Exception) {}
 
@@ -124,13 +149,36 @@ class MainActivity : AppCompatActivity() {
         try {
             if (board[row - 1][column] == 1) {
                 findViewById<TextView>(boxList[row - 1][column]).setBackgroundColor(Color.BLACK)
+                board[row - 1][column] = 0
             } else {
                 findViewById<TextView>(boxList[row - 1][column]).setBackgroundColor(Color.WHITE)
+                board[row - 1][column] = 1
             }
         }catch (e: Exception) {}
 
+    }
 
+    private fun retry(board: Array<Array<Int>>, boxList: List<List<Int>>) {
+        for(row: Int in 0..4) {
+            for(column: Int in 0..4) {
+                board[row][column] = 1
+                findViewById<TextView>(boxList[row][column]).setBackgroundColor(Color.WHITE)
+            }
+        }
 
+        numOfClicks = 0
+        showCount.setText(numOfClicks.toString())
+    }
+
+    private fun winChecker(board: Array<Array<Int>>): Int {
+        for(i in 0..4) {
+            for(j in 0..4) {
+                if(board[i][j] == 1) {
+                    return 0
+                }
+            }
+        }
+        return 1
 
     }
 
